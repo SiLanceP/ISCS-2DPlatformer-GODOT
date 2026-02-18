b3634e173512db00fbeaabd9719167b3314c54e5
SiLanceP <112547328+SiLanceP@users.noreply.github.com>
2026-02-18 21:03:23 +0800

    Added Double jump, moving platforms, animations
    added double jump for the character
    added a moving platform (its a separate scene)
    added animations (integrated the falling animation)

--- 2d-platformer/scripts/player.gd
+++ 2d-platformer/scripts/player.gd
@@ -4,27 +4,40 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -900.0
+const max_jumps = 2
+var jump_count = 0
 
 
 func _physics_process(delta: float) -> void:
 	
 	# animations
-	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "run"
+	if is_on_floor():
+		if velocity.x > 1 or velocity.x < -1:
+			animated_sprite_2d.play("run")
+		else:
+			animated_sprite_2d.play("idle")
+
 	else:
-		animated_sprite_2d.animation = "idle"
-	
-	# Add the gravity.
-	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animated_sprite_2d.animation = "idle"
+		
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		elif velocity.y > 0:
+			animated_sprite_2d.play("fall")
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		jump_count = 0
+	
+	if Input.is_action_just_pressed("jump"):
+		if jump_count < max_jumps:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+			animated_sprite_2d.play("jump") #replays the jump animation mid-air
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -36,3 +49,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction == -1.0:
 		animated_sprite_2d.flip_h = true
+	
+	#One-way platforms
+	if is_on_floor() and Input.is_action_just_pressed("down"):
+		position.y += 1.0

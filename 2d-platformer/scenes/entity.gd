1ba55f11e30ee1800bd93b49650cece0a37d1069
SiLanceP <112547328+SiLanceP@users.noreply.github.com>
2026-02-20 15:36:57 +0800

    added a little creature
    this entity plays audio when being bounced (possibly last addition)

---
+++ 2d-platformer/scenes/entity.gd
@@ -0,0 +1,24 @@
+extends Area2D
+@onready var sounds = [$speaki, $uaa, $cuayo]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		
+		# Check if the player is falling DOWNWARD onto the entity
+		if body.velocity.y > 0:
+			
+			#Make the player bounce
+			body.velocity.y = body.JUMP_VELOCITY 
+			
+			#play a random sound
+			var random_sound = sounds.pick_random()
+			random_sound.play()
